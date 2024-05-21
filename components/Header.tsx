51a7e35286d3c0194a8a1652627abaf0fd907435
Dwcks Dinh <corclan19@gmail.com>
2024-05-21 21:39:55 +0700

    has bug i cant serve image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,35 @@
 import { html } from "hono/html"
+import Button from "@components/Button"
 
-const Header = () => html`
-    <p>For B2B Businesses</p>
-    <h2>
-        We Connect You With 
-        Your Ideal Customer -
-        You Do The Rest.
-    </h2>
-    <p>
-        Supercharge your pipeline with prospects that 
-        need what you offer.
-    </p>
+interface props {
+    children?: any
+}
+
+const Head = (props: props) => html`
+    <div>
+        <p>For B2B Businesses</p>
+        <h2>
+            We Connect You With
+            Your Ideal Customer -
+            You Do The Rest.
+        </h2>
+        <p>
+            Supercharge your pipeline with prospects that
+            need what you offer.
+        </p>
+        ${props.children}
+    </div>
 `
+
+const Head2 = (props: props) => <Head>
+        <Button/>
+        {props.children}
+    </Head>
+
+const Header = () => <Head2>
+    <div>
+        <img src="/images/Business-Growth-illustration-1.png"/>
+    </div>
+</Head2>
 
 export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 import Layout from '@components/Layout'
 import { SiteData } from '@components/Layout'
 import Navbar from "@components/NavBar"
 import Button from "@components/Button"
+import Header from "@components/Header"
 
 const app = new Hono()
 
+app.use('/static/*', serveStatic({ root: '../public/images' }))
+
 const Content = (props: { siteData: SiteData; name: string }) => (
   <Layout {...props.siteData}>
+    <Header></Header>
     <Navbar></Navbar>
     <h1>Hello {props.name}</h1>
     <Button></Button>
@@ -23,5 +28,13 @@ app.get('/', (c) => {
   }
   return c.html(<Content {...props} />)
 })
+
+app.get(
+  '/static/*',
+  serveStatic({
+    root: '../public/images',
+    rewriteRequestPath: (path) => path.replace(/^\/static/, '../public/images'),
+  })
+)
 
 export default app
